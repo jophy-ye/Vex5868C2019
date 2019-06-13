8aacfdefa8ffcd7af3a17eba624bc15b065c8a26
jophy <jophyyjh@gmail.com>
2019-06-13 23:17:00 +0800

    Finish function templates of the class "RobotMovement" inside include/RobotMovement.h

--- include/Constants.h
+++ include/Constants.h
@@ -1,9 +1,43 @@
-#ifndef CONSTANTS_H_
-#define CONSTANTS_H_
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
 
-namespace Const
+#include "api.h"
+
+namespace CONSTANTS
 {
+    namespace PORT
+    {
+        // all not set +++++++++++++++++++++++
+        // all port numbers
+        const unsigned int LEFT_FRONT       = 0;
+        const unsigned int RIGHT_FRONT      = 0;
+        const unsigned int LEFT_BACK        = 0;
+        const unsigned int RIGHT_BACK       = 0;
+    }
 
+    namespace ROBOT
+    {
+        // all not set +++++++++++++++++++++++
+        // all dimensions for robot
+        const double WHEEL_DIAMETER         = 0;
+    }
+
+    namespace PID
+    {
+        // all values for PID control
+    }
+
+    namespace BASIS_MOTORS
+    {
+        // all not set +++++++++++++++++++++++
+        // all info about basis motors (except ports)
+        const pros::motor_gearset_e_t GEARSET = pros::E_MOTOR_GEARSET_18;
+
+        const bool LEFT_FRONT_REVERSED      = false;
+        const bool RIGHT_FRONT_REVERSED     = false;
+        const bool LEFT_BACK_REVERSED       = false;
+        const bool RIGHT_BACK_REVERSED      = false;
+    }
 }
 
 #endif
--- include/RobotMovement.h
+++ include/RobotMovement.h
@@ -1,14 +1,79 @@
-#ifndef ROBOT_MOVEMENT_H_
-#define ROBOT_MOVEMENT_H_
+#ifndef _ROBOT_MOVEMENT_H_
+#define _ROBOT_MOVEMENT_H_
 
-#include "./Constants.h"
-#include "main.h"
+#include "Constants.h"
+using namespace CONSTANTS;
+#include "api.h"
 
 class RobotMovement
 {
 private:
+    pros::Motor LeftFrontMotor;
+    pros::Motor LeftBackMotor;
+    pros::Motor RightFrontMotor;
+    pros::Motor RightBackMotor;
 public:
+    RobotMovement():
+       LeftFrontMotor(PORT::LEFT_FRONT, BASIS_MOTORS::GEARSET, BASIS_MOTORS::LEFT_FRONT_REVERSED, pros::E_MOTOR_ENCODER_DEGREES),
+       LeftBackMotor(PORT::LEFT_BACK, BASIS_MOTORS::GEARSET, BASIS_MOTORS::LEFT_BACK_REVERSED, pros::E_MOTOR_ENCODER_DEGREES),
+       RightFrontMotor(PORT::RIGHT_FRONT, BASIS_MOTORS::GEARSET, BASIS_MOTORS::RIGHT_FRONT_REVERSED, pros::E_MOTOR_ENCODER_DEGREES),
+       RightBackMotor(PORT::RIGHT_BACK, BASIS_MOTORS::GEARSET, BASIS_MOTORS::RIGHT_BACK_REVERSED, pros::E_MOTOR_ENCODER_DEGREES) {};
+    /*
+    The constructor function of the class "RobotMovement"
 
+    Input: (none)
+    Output: (none)
+    */
+
+    void Stop();
+    /*
+    Function for the whole robot to stop
+
+    Input: (none)
+    Output: (none)
+    */
+
+    void Move(double power = 127);
+    /*
+    Function for the whole robot to start moving
+
+    Input:
+        power (double)      :  The power for the motor (-127 ~ 127)
+    Output: (none)
+    */
+
+    void MoveDistance(double dist, double power = 127, bool OptimizedStop = false);
+    /*
+    Function for the whole robot to move a certain distance
+
+    Input:
+        dist (double)       :  The desire distance in centimeters(cm)
+        power (double)      :  The power for the motor (-127 ~ 127)
+        OptimizedStop (bool):  The stop of the movement will be more accurate if true
+    Output: (none)
+    */
+
+    void SlideDistance(double dist, double power = 127, bool OptimizedStop = false);
+    /*
+    Function for the whole robot to slide left and right
+
+    Input:
+        dist (double)       :  The desire distance in centimeters(cm)
+        power (double)      :  The power for the motor (-127 ~ 127)
+        OptimizedStop (bool):  The stop of the movement will be more accurate if true
+    Output: (none)
+    */
+
+    void Turn(double degree, double power = 127, bool OptimizedStop = false);
+    /*
+    Function for the whole robot to turn
+
+    Input:
+        degree (double)     :  The desire degree
+        power (double)      :  The power for the motor (-127 ~ 127)
+        OptimizedStop (bool):  The stop of the movement will be more accurate if true
+    Output: (none)
+    */
 };
 
 #endif
